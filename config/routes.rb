4ec3898f407b76c04c942a29796202ad10e38fe5
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-02-15 23:53:35 +0200

    root page correct

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'store/index'
+  get "store/index"
   
 
   resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
