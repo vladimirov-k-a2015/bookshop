7fcca7f6983c18548e5e901f906b03af88840e4c
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-02-15 23:58:28 +0200

    root page correct - 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
 
   get "store/index"
   
+  # You can have the root of your site routed with "root"
+  root to:'store#index', as: 'store'
 
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root to:'store#index', as: 'store'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
