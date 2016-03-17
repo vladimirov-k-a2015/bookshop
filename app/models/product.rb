a9dc668a79a74f4b6a0ec927c1c62be6a4b25fc0
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-03-17 11:28:44 +0200

    Part 8 - finished

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,9 @@ class Product < ActiveRecord::Base
 		message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
 	}
 
+	def self.latest
+		Product.order(:updated_at).last
+	end
+
+
 end
